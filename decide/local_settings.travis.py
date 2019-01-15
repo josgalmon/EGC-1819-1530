0743ae500f5ec5cb7975ddddd61c7c0b0c332f0e
josgalmon <josgalmon1@alum.us.es>
2019-01-15 16:06:51 +0100

    Añadido travis
    Añadidos los archivos necesarios para la integración con travis

---
+++ decide/local_settings.travis.py
@@ -0,0 +1,33 @@
+ALLOWED_HOSTS = ["*"]
+
+# Modules in use, commented modules that you won't use
+MODULES = [
+    'store'
+]
+
+APIS = {
+    'authentication': 'http://localhost:8000',
+    'base': 'http://localhost:8000',
+    'booth': 'http://localhost:8000',
+    'census': 'http://localhost:8000',
+    'mixnet': 'http://localhost:8000',
+    'postproc': 'http://localhost:8000',
+    'store': 'http://localhost:8000',
+    'visualizer': 'http://localhost:8000',
+    'voting': 'http://localhost:8000',
+}
+
+BASEURL = 'http://localhost:8000'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# number of bits for the key, all auths should use the same number of bits
+KEYBITS = 256
